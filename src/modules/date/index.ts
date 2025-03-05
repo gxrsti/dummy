3d2e4683ca87fba580c2d0c830f4cb4047f58bcd
Gersti <patrick.gerstacker@gmail.com>
2025-03-05 11:14:45 +0100

    fix: add future + past date range

--- src/modules/date/index.ts
+++ src/modules/date/index.ts
@@ -3,6 +3,8 @@ interface BetweenOptions {
   to: string | Date | number;
 }
 
+const YearDifference = 40;
+
 export class DateModule {
   private static getRandomTimeInRange(from: number, to: number): number {
     if (from > to) throw new Error("'from' date must be earlier than 'to' date.");
@@ -15,16 +17,29 @@ export class DateModule {
     return time;
   }
 
+  private static getCurrentDateWithYearDifference(difference: number): number {
+    const futureDate = new Date(Date.now());
+    futureDate.setFullYear(futureDate.getFullYear() + difference);
+    return futureDate.getTime();
+  }
+
   past(): Date {
     return new Date(DateModule.getRandomTimeInRange(0, Date.now()));
   }
 
   future(): Date {
-    return new Date(DateModule.getRandomTimeInRange(Date.now(), Number.MAX_SAFE_INTEGER));
+    return new Date(
+      DateModule.getRandomTimeInRange(Date.now(), DateModule.getCurrentDateWithYearDifference(YearDifference)),
+    );
   }
 
   anytime(): Date {
-    return new Date(DateModule.getRandomTimeInRange(-Number.MAX_SAFE_INTEGER, Number.MAX_SAFE_INTEGER));
+    return new Date(
+      DateModule.getRandomTimeInRange(
+        DateModule.getCurrentDateWithYearDifference(-YearDifference),
+        DateModule.getCurrentDateWithYearDifference(YearDifference),
+      ),
+    );
   }
 
   between(options: BetweenOptions): Date {
